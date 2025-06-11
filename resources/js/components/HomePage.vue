a12028bb7d537aad779c11ab2336328253c7ad22
Qalaudev <maksatdaniarbekly@gmail.com>
2025-06-12 04:53:05 +0500

    fix: skryt knokpa

--- resources/js/components/HomePage.vue
+++ resources/js/components/HomePage.vue
@@ -90,7 +90,7 @@
 
 
     <!-- КӨМЕК ҚАЖЕТ ЕТЕТІН ЖАНДАР -->
-    <section class="py-20 px-4 md:px-8 bg-white relative">
+    <section v-show="komek" class="py-20 px-4 md:px-8 bg-white relative">
         <div class="max-w-7xl mx-auto">
             <h2 class="text-4xl font-bold mb-14 text-center text-green-700 animate-fade-in-up">
                 {{ $t('people_in_need') }}
@@ -433,6 +433,7 @@ export default {
 
     data() {
         return {
+            komek:false,
             groups:[],
             people:[],
             isVisible: false,
